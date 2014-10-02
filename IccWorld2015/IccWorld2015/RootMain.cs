0854122f9eca398529b87240e8f7bacb54499b10
Saad Mahmood <saad-mehmood@outlook.com>
2014-10-02 05:00:56 +0500

    mainlayout

---
+++ IccWorld2015/IccWorld2015/RootMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IccWorld2015
+{
+    class RootMain
+    {
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public string Batting { get; set; }
+        public string Bowling { get; set; }
+        public string Batsman1 { get; set; }
+        public string Batsman2 { get; set; }
+        public string Bowler1 { get; set; }
+        public string Bowler2 { get; set; }
+        public string Over { get; set; }
+        public string Required { get; set; }
+        public string Overs { get; set; }
+        public string Team1W { get; set; }
+        public string Team2W { get; set; }
+        public string Favourite { get; set; }
+        public string Venue { get; set; }
+        public string Type { get; set; }
+        public string Batsman1Scr { get; set; }
+        public string Batsman2Scr { get; set; }
+        public string Bowler1Scr { get; set; }
+        public string PlayersToWatch { get; set; }
+        public string Final { get; set; }
+    }
+}
